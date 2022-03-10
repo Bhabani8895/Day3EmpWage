924552a2b59cfce6f8c12443ef90a14a9b60d9d7
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2022-03-10 18:07:26 +0530

    Daily wage calculated

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -9,17 +9,25 @@ namespace EmployeeWage
             Console.WriteLine("Welcome to Employee Wage Program");
 
             int isPresent = 1;
+            int empWage = 0;
+            int ratePerHrs = 20;
+            int empHrs = 0;
             Random random = new Random();
             int randominput = random.Next(0,2);
 
             if (randominput == isPresent)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else 
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = ratePerHrs * empHrs;
+            Console.WriteLine("Employee wage per day: " + empWage);
+           
         }
     }
 }
