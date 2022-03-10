aeb4462c0d42fd3aaa0be5e2a9684d14ed431c57
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2022-03-10 17:38:16 +0530

    Employee attandance has been checked

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,6 +7,19 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Program");
+
+            int isPresent = 1;
+            Random random = new Random();
+            int randominput = random.Next(0,2);
+
+            if (randominput == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else 
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
