c0a4d894b9aced08d9ff36c099feb471f3f24b55
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2022-03-10 17:06:39 +0530

    welcome message added

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class EmpWage
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Program");
+        }
+    }
+}
