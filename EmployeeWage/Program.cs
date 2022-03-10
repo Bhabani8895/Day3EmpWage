7b580da19da1fcefd42046f9ac7d6845317ea9be
[Bhabani8895] <[bhabaniprof2495@gmail.com]>
2022-03-10 22:23:47 +0530

    TOtal 100 hour has been added for monthly wage

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -8,16 +8,20 @@ namespace EmployeeWage
         public const int is_Part_Time = 2;
         public const int ratePerHrs = 20;
         public const int max_Working_Days = 20;
+        public const int max_Working_Hrs = 100;
 
         public static void Main(string[] args)
         {
             int empWage = 0;
             int empHrs = 0;
             int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalDays = 1;
             string typeOfEmployee = "";
-          
+
+
             Random employee = new Random();
-            for (int day = 1; day <= max_Working_Days; day++)
+            while (totalEmpHrs <= max_Working_Hrs && totalDays <= max_Working_Days)
             {
                 int randomInput = employee.Next(0, 3);
 
@@ -38,11 +42,15 @@ namespace EmployeeWage
                         typeOfEmployee = "Absent";
                         break;
                 }
+                totalEmpHrs = totalEmpHrs + empHrs;
                 empWage = ratePerHrs * empHrs;
-                Console.WriteLine("{0}Emplyee of day: {1}wage is {2}", typeOfEmployee, day, empWage);
+                totalEmpWage = ratePerHrs * totalEmpHrs;
+                Console.WriteLine("{0}Emplyee of day: {1}wage is {2}", typeOfEmployee, totalDays, empWage);
                 totalEmpWage = totalEmpWage + empWage;
+                totalDays++;
             }
             Console.WriteLine("Employee wage for {0} day is : {1}", max_Working_Days, totalEmpWage);
+          
         }
     }
 }
